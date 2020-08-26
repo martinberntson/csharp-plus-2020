b0d1b30cda4d50bb6e22dd710516d83dc776b1f0
ASUS-ENTERPRISE\tempo <temporal@live.se>
2020-08-26 10:53:09 +0200

    sync

--- Session02/Session02/Session02Excercise1/Program.cs
+++ Session02/Session02/Session02Excercise1/Program.cs
@@ -7,6 +7,10 @@ namespace Session02Excercise1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello scrubs");
+
+            string stringValue = "of Steel";
+
+            Console.WriteLine("The walls are made " + stringValue);
         }
 
     }
