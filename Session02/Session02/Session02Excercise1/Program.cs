290b3a343a73935c2de12633c94c2f000ed54ac3
ASUS-ENTERPRISE\tempo <temporal@live.se>
2020-08-26 10:39:22 +0200

    restored

--- Session02/Session02/Session02Excercise1/Program.cs
+++ Session02/Session02/Session02Excercise1/Program.cs
@@ -7,14 +7,6 @@ namespace Session02Excercise1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello scrubs");
-            if (1 > 2)
-            {
-                Console.WriteLine("One is greater than two");
-            }
-            else
-            {
-                Console.WriteLine("Two is greater than one");
-            }
         }
 
     }
