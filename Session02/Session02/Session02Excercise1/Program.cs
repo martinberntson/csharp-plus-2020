fc4a77800d77cf2d22cb4a4bb43cba7ed0ea4273
ASUS-ENTERPRISE\tempo <temporal@live.se>
2020-08-26 11:53:31 +0200

    Final

--- Session02/Session02/Session02Excercise1/Program.cs
+++ Session02/Session02/Session02Excercise1/Program.cs
@@ -7,14 +7,26 @@ namespace Session02Excercise1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello scrubs");
+            Console.WriteLine("Please enter a value:");
 
-            var integerValue = 5;
+            var integerValue = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Integer's value is: " + integerValue);
 
             string stringValue = "of Steel";
 
             Console.WriteLine("The walls are made " + stringValue);
+            if (integerValue > 90)
+            {
+                Console.WriteLine(integerValue + " is over 90.");
+            }
+            else if (integerValue == 90){
+                Console.WriteLine(integerValue + " is equal to 90.");
+            }
+            else
+            {
+                Console.WriteLine(integerValue + " is under 90.");
+            }
         }
 
     }
